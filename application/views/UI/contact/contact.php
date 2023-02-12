5923b2015e9ee6110551c685bd3fe59b419247be
lalitTeq <lalit.meshram@teqfocus.com>
2023-02-12 18:32:43 +0530

    hotfix->1.0 contact update120223

--- application/views/UI/contact/contact.php
+++ application/views/UI/contact/contact.php
@@ -108,7 +108,7 @@ page-title -->
     </div>
     <div class="row mt-60">
       <div class="col-lg-12">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d232.6282660584311!2d78.9841141!3d21.1104827!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4954f04544f25%3A0x9aae053275193339!2sDiyewar%20Engineering!5e0!3m2!1sen!2sin!4v1676206137143!5m2!1sen!2sin" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d232.6282660584311!2d78.9841141!3d21.1104827!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4954f04544f25%3A0x9aae053275193339!2sDiyewar%20Engineering!5e0!3m2!1sen!2sin!4v1676206137143!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <!--div style="width: 100%; height: 300px;" id="map-02" class="g-map" data-type='green'></div-->
       </div>
     </div>
